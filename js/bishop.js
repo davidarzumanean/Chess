a6dfbec8084871cd1d4548ac5a85bcb90acf110a
davidarzumanean <davidarzumanean@gmail.com>
2017-08-21 20:17:15 +0400

    general commit

--- js/bishop.js
+++ js/bishop.js
@@ -1,14 +1,15 @@
 function Bishop(color, coords) {
     Piece.call(this, color, coords);
+
+    this.validateMove = function (dropCoords) {
+        var currentCoordsArr = this.coords;
+        var currentCoords = {};
+        currentCoords.x = currentCoordsArr[0];
+        currentCoords.y = currentCoordsArr[1];
+
+
+    }
 }
 
 Bishop.prototype = new Piece();
 Bishop.prototype.constructor = Bishop;
-
-Bishop.validateMove = function (dropCoords) {
-    var currentCoords = this.coords;
-    var pieceColor = this.color;
-
-   
-
-};
