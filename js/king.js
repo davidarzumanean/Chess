59a7ee47f785a4b31c37ee5bc6a873f8b80fe2e9
David Arzumanyan <davidarzumanean@gmail.com>
2017-08-23 22:08:52 +0400

    king move validation

--- js/king.js
+++ js/king.js
@@ -10,5 +10,21 @@ function King(color, coords) {
         currentCoords.x = currentCoordsArr[0];
         currentCoords.y = currentCoordsArr[1];
 
+        var currentXCoordindex = xCoordIndexCoordination[currentCoords.x];
+        var dropXCoordindex = xCoordIndexCoordination[dropCoords.x];
+
+        if (dropCoords.y === currentCoords.y + 1 || dropCoords.y === currentCoords.y - 1) {
+            if (chess.board[dropCoords.y][dropCoords.x] !== null) {
+                return {success: true, eat: true};
+            }
+            return {success: true};
+        } else if (dropXCoordindex === currentXCoordindex + 1 || dropXCoordindex === currentXCoordindex - 1){
+            if (chess.board[dropCoords.y][dropCoords.x] !== null) {
+                return {success: true, eat: true};
+            }
+            return {success: true};
+        }
+
+        return {success: false}
     };
 }
